bcbf7fbf3507256f851aa8464d1fa25cbe03b177
Paul Alarcon <pa.f.e@hotmail.com>
2020-08-23 23:33:48 -0400

    added new features

---
+++ scripts/transitions.js
@@ -0,0 +1,34 @@
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector("header");
+    header.classList.toggle('versatile', window.scrollY  > 0)
+    console.log(window.scrollY)
+    const lastOne = document.querySelector("#projects");
+    console.log(lastOne)
+    header.classList.toggle('puff', window.scrollY  >= lastOne.offsetTop)
+})
+
+ScrollReveal().reveal("#home", {
+    delay: 400,
+    distance: "100px",
+    duration: 1000,
+    easing: "ease-in",
+    origin: "left",
+  });
+  
+  ScrollReveal().reveal("#about", {
+    delay: 400,
+    distance: "100px",
+    duration: 1000,
+    easing: "ease-in",
+    origin: "right",
+  });
+
+  ScrollReveal().reveal("#projects", {
+    delay: 400,
+    distance: "100px",
+    duration: 1000,
+    easing: "ease-in",
+    origin: "bottom",
+  });
+
